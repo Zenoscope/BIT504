aafdb41c4c876659823b2bbc5b06cc6799076c2e
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-16 12:29:55 +1300

    Update Paddle.java

--- Assignment 3/Paddle.java
+++ Assignment 3/Paddle.java
@@ -6,13 +6,19 @@ public class Paddle extends Sprite {
 	
 	public Paddle() {
 		// TODO: Set width to Settings.PADDLE_WIDTH
+		self.width = Settings.PADDLE_WIDTH;
 		// TODO: Set width to Settings.PADDLE_HEIGHT
-		// TODO: Call resetPosition
+		self.height = Settings.PADDLE_HEIGHT;
+		// DONE: Call resetPosition
+		resetposition();
 	}
 	
 	public void resetPosition() {
 		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
+		x = Settings.INITIAL+PADDLE_X;
+		y = Settings.INITIAL+PADDLE_Y;
 		// Note: Check Ball.java for a hint
+		
 	}
 	
 	public void update() {
@@ -20,6 +26,8 @@ public class Paddle extends Sprite {
 		
 		// TODO: Prevent the paddle from moving outside of the screen
 		// This can be done using two if statements (one for the left side of the screen and one for the right)
+		if (x < 0) { x = 0;}
+		else if (x > settings.SCREEN_WIDTH) { X = Settings.SCREEN_WIDTH; }
 
 	}
 	
@@ -29,5 +37,6 @@ public class Paddle extends Sprite {
 	
 	public void setXVelocity(int vel) {
 		// TODO: Set x velocity
+		xVelocity = 1;
 	}
 }
