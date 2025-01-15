34abe0003999758e0992085c82e8ec709a99dd52
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-15 21:32:33 +1300

    Update BreakoutPanel.java

--- Assignment 3/BreakoutPanel.java
+++ Assignment 3/BreakoutPanel.java
@@ -30,8 +30,11 @@ public class BreakoutPanel extends JPanel implements ActionListener, KeyListener
 		timer.start();
 		
 		// TODO: Create a new ball object and assign it to the appropriate variable
+		ball = new Ball;
 		// TODO: Create a new paddle object and assign it to the appropriate variable
+		
 		// TODO: Create a new bricks array (Use Settings.TOTAL_BRICKS)
+		
 		// TODO: Call the createBricks() method
 	}
 	
