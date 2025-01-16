cb6abdd34d13687b699ebee3be5625fa983b121d
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-16 22:01:49 +1300

    Update Brick.java

--- Assignment 3/Brick.java
+++ Assignment 3/Brick.java
@@ -1,30 +1,42 @@
 import java.awt.Graphics;
 
-public class Brick extends Sprite {
-	
-	private boolean broken = false;
-	
-	public Brick(int x, int y) {
-		// TODO: Set x using the parameter
-		self.x = x;
-		// TODO: Set y using the parameter
-		self.y = y;
-		// TODO: Set the width and height of the brick using Settings.BRICK_WIDTH/HEIGHT
-		self.WIDTH = Settings.BRICK_WIDTH;
-		self.HEIGHT = Settings.BRICK_HEIGHT;
-	}
+public class Paddle extends Sprite {
 
-	public boolean isBroken() {
-		return broken;	// DONE: Return the correct variable
+	private int xVelocity;
+	
+	public Paddle() {
+		// TODO: Set width to Settings.PADDLE_WIDTH
+		self.width = Settings.PADDLE_WIDTH;
+		// TODO: Set width to Settings.PADDLE_HEIGHT
+		self.height = Settings.PADDLE_HEIGHT;
+		// DONE: Call resetPosition
+		resetposition();
 	}
-	public void setBroken(boolean b) {
-		// TODO: Set the broken variable using the parameter given
-		broken = b;
+	
+	public void resetPosition() {
+		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
+		x = Settings.INITIAL+PADDLE_X;
+		y = Settings.INITIAL+PADDLE_Y;
+		// Note: Check Ball.java for a hint
+		resetPosition();
+	}
+	
+	public void update() {
+		x += xVelocity;
+		
+		// TODO: Prevent the paddle from moving outside of the screen
+		// This can be done using two if statements (one for the left side of the screen and one for the right)
+		if (x < 0) { x = 0;}
+		else if (x > settings.SCREEN_WIDTH) { X = Settings.SCREEN_WIDTH; }
+
 	}
 	
 	public void paint(Graphics g) {
-		if(!broken) {
-			g.fillRect(x, y, Settings.BRICK_WIDTH, Settings.BRICK_HEIGHT);
-		}
+		g.fillRect(x, y, Settings.PADDLE_WIDTH, Settings.PADDLE_HEIGHT);
+	}
+	
+	public void setXVelocity(int vel) {
+		// TODO: Set x velocity
+		xVelocity = 1;
 	}
 }
