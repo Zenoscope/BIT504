e7e8fda8ed267a6112db937c16318b963d3dba58
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-16 21:59:53 +1300

    Update Breakout.java

--- Assignment 3/Breakout.java
+++ Assignment 3/Breakout.java
@@ -11,14 +11,14 @@ public class Breakout extends JFrame{
 	public Breakout() {
 		// DONE: Set the size of the screen (use Settings.WINDOW_WIDTH/HEIGHT)		
                 setSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);                
-             	// adds the panel to the frame		
+             	// adds the panel to the frame
              	add(new BreakoutPanel());
 		// DONE: Set the title
-		setTitle(Settings.WINDOW_TITLE);
+		setTitle(Settings.WINDOW_NAME);
 		// DONE: Set the background colour to white		
 		setBackground(Color.WHITE);
 		// DONE: Set resizable to false
-		setResizable(Settings.);
+		setResizable(false);
         	setDefaultCloseOperation(EXIT_ON_CLOSE);
         	panel = new BreakoutPanel(this);
         	add(panel);
