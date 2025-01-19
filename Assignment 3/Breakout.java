36559513daa125efb7dc9720f4176823d7bc1fb6
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-20 10:17:03 +1300

    Update Breakout.java
    Monday updates

--- Assignment 3/Breakout.java
+++ Assignment 3/Breakout.java
@@ -10,19 +10,19 @@ public class Breakout extends JFrame{
 	
 	public Breakout() {
 		// DONE: Set the size of the screen (use Settings.WINDOW_WIDTH/HEIGHT)		
-                setSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);                
-             	// adds the panel to the frame
+        setSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);                
+       	// adds the panel to the frame
 		// FIXME
-             	add(new BreakoutPanel());		
+        // add(new BreakoutPanel());		
 		// DONE: Set the title
 		setTitle(Settings.WINDOW_NAME);
 		// DONE: Set the background colour to white		
 		setBackground(Color.WHITE);
 		// DONE: Set resizable to false
 		setResizable(false);
-        	setDefaultCloseOperation(EXIT_ON_CLOSE);
-        	panel = new BreakoutPanel(this);
-        	add(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new BreakoutPanel(this);
+        add(panel);
 		// DONE: Set visible to true
 		setVisible(true);
 	}
@@ -30,7 +30,7 @@ public class Breakout extends JFrame{
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	         public void run() {
-	        	 new Breakout();	
+	        	 new Breakout();
 	         }
 		});
 
