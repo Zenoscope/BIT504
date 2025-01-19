a26deab25a176bf35ad93d27123297342bc8ff32
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-20 10:18:28 +1300

    Update Brick.java
    Monday update

--- Assignment 3/Brick.java
+++ Assignment 3/Brick.java
@@ -1,42 +1,36 @@
 import java.awt.Graphics;
 
-public class Paddle extends Sprite {
-
-	private int xVelocity;
+public class Brick extends Sprite {
 	
-	public Paddle() {
-		// TODO: Set width to Settings.PADDLE_WIDTH
-		self.width = Settings.PADDLE_WIDTH;
-		// TODO: Set width to Settings.PADDLE_HEIGHT
-		self.height = Settings.PADDLE_HEIGHT;
-		// DONE: Call resetPosition
-		resetposition();
+	private boolean broken = false;
+	
+	public Brick(int x, int y) {
+		// TODO: Set x using the parameter
+		this.x = x;
+		// TODO: Set y using the parameter
+		this.y = y;
+		// TODO: Set the width and height of the brick using Settings.BRICK_WIDTH/HEIGHT
+		this.width = Settings.BRICK_WIDTH;
+		this.height = Settings.BRICK_HEIGHT;
 	}
-	
-	public void resetPosition() {
-		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
-		x = Settings.INITIAL+PADDLE_X;
-		y = Settings.INITIAL+PADDLE_Y;
-		// Note: Check Ball.java for a hint
-		resetPosition();
-	}
-	
-	public void update() {
-		x += xVelocity;
-		
-		// TODO: Prevent the paddle from moving outside of the screen
-		// This can be done using two if statements (one for the left side of the screen and one for the right)
-		if (x < 0) { x = 0;}
-		else if (x > settings.SCREEN_WIDTH) { X = Settings.SCREEN_WIDTH; }
 
+	public boolean isBroken() {
+		// TODO: Return the correct variable
+		return broken;
+	}
+	public void setBroken(boolean b) {
+		// TODO: Set the broken variable using the parameter given
+		if (b == true) {
+		 	this.broken = true;
+		}
+		else if (b == false) {
+		 	this.broken = false;
+		}
 	}
 	
 	public void paint(Graphics g) {
-		g.fillRect(x, y, Settings.PADDLE_WIDTH, Settings.PADDLE_HEIGHT);
-	}
-	
-	public void setXVelocity(int vel) {
-		// TODO: Set x velocity
-		xVelocity = 1;
+		if(!broken) {
+			g.fillRect(x, y, Settings.BRICK_WIDTH, Settings.BRICK_HEIGHT);
+		}
 	}
 }
