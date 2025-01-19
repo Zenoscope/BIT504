8cb167c9ef9b3debbc0adc7cbe4825461b726726
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-20 10:18:55 +1300

    Update Paddle.java
    Monday update

--- Assignment 3/Paddle.java
+++ Assignment 3/Paddle.java
@@ -6,18 +6,18 @@ public class Paddle extends Sprite {
 	
 	public Paddle() {
 		// TODO: Set width to Settings.PADDLE_WIDTH
-		self.width = Settings.PADDLE_WIDTH;
+		this.width = Settings.PADDLE_WIDTH;
 		// TODO: Set width to Settings.PADDLE_HEIGHT
-		self.height = Settings.PADDLE_HEIGHT;
+		this.height = Settings.PADDLE_HEIGHT;
 		// DONE: Call resetPosition
-		resetposition();
+		resetPosition();
 	}
 	
 	public void resetPosition() {
 		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
-		x = Settings.INITIAL+PADDLE_X;
-		y = Settings.INITIAL+PADDLE_Y;
 		// Note: Check Ball.java for a hint
+		this.x = Settings.INITIAL_PADDLE_X;
+		this.y = Settings.INITIAL_PADDLE_Y;
 		
 	}
 	
@@ -26,8 +26,17 @@ public class Paddle extends Sprite {
 		
 		// TODO: Prevent the paddle from moving outside of the screen
 		// This can be done using two if statements (one for the left side of the screen and one for the right)
-		if (x < 0) { x = 0;}
-		else if (x > settings.SCREEN_WIDTH) { X = Settings.SCREEN_WIDTH; }
+		// Stop on left side of screen
+		if(x <= 0 + Settings.PADDLE_WIDTH) {
+			// DONE: Set x to 0 so it does not leave the screen
+			x = 0 + Settings.PADDLE_WIDTH;
+		}
+		
+		// Stop on right side of screen
+		if(x >= Settings.WINDOW_WIDTH - Settings.PADDLE_WIDTH) {
+			// DONE: Set x to the right edge of the screen (see the above if condition)
+			x = Settings.WINDOW_WIDTH - Settings.PADDLE_WIDTH;
+		}
 
 	}
 	
@@ -37,6 +46,6 @@ public class Paddle extends Sprite {
 	
 	public void setXVelocity(int vel) {
 		// TODO: Set x velocity
-		xVelocity = 1;
+		this.xVelocity = vel;
 	}
 }
