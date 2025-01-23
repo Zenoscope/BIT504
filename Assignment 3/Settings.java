8b3474fc5d8f528ae6df383649e72de74f6ac697
Ryan McArthur <zenoscope@rocketmail.com>
2025-01-23 22:32:47 +1300

    Update Settings.java

--- Assignment 3/Settings.java
+++ Assignment 3/Settings.java
@@ -8,8 +8,8 @@ public class Settings {
 	public static final int BRICK_HEIGHT = 30;
 	public static final int BRICK_HORI_PADDING = 50;
 	public static final int BRICK_WIDTH = 70;
-	public static final int INITIAL_BALL_X = 150;
-	public static final int INITIAL_BALL_Y = 350;
+	public static final int INITIAL_BALL_X = 150; //150
+	public static final int INITIAL_BALL_Y = 350; //350
 	public static final int INITIAL_PADDLE_X = 100;
 	public static final int INITIAL_PADDLE_Y = 370;
 	public static final int PADDLE_WIDTH = 100;
@@ -17,7 +17,16 @@ public class Settings {
 	public static final int PADDLE_VELOCITY = 2;
 	public static final int BALL_WIDTH = 10;
 	public static final int BALL_HEIGHT = 10;
+	public static final int BALL_X_VELOCITY = 1; //1
+	public static final int BALL_Y_VELOCITY = 1; //1
 	public static final int MESSAGE_POSITION = 270;
 	public static final int LIVES_POSITION_X = 5;
 	public static final int LIVES_POSITION_Y = 15;
-}
+	
+	// change the number of lives
+	public static final int NUM_LIVES = 1;
+	// not used, but could be with updates
+	public static final int START_LEVEL = 1;
+	// set the top of the playing field
+	public static final int TOP_OF_PLAYFIELD = 30;
+	}
